f8c9b7648900cac3c0df7fbeabd79a8581aaa547
Dahlqvist <fpd09@ic.ac.uk>
2019-05-03 16:02:47 +0100

    maths and code for sum followed by test

--- Code/main.py
+++ Code/main.py
@@ -1,7 +1,13 @@
 import simple_tests
 
-test1=simple_tests.TestUniformVariable(0,100,32,10)
+# test1=simple_tests.TestUniformVariable(0,100,32,10)
+# test1.plot_against_precision(4,32)
+# test1.precision=10
+# test1.plot_against_threshold()
 
-test1.plot_against_precision(4,32)
-test1.precision=10
-test1.plot_against_threshold()
+test1=simple_tests.TestUniformVariable(0,2,0.25,10)
+test1.compute()
+test2=simple_tests.TestSumUniformVariable(0,1,0,1,0.25,10)
+test2.compute()
+print(test1.error_prob)
+print(test2.error_prob)
--- Code/simple_tests.py
+++ Code/simple_tests.py
@@ -45,3 +45,25 @@ class TestUniformVariable:
         plt.xlabel('threshold')
         plt.ylabel('probability of error (%)')
         plt.show()
+
+class TestSumUniformVariable:
+
+    def __init__(self, lowerbound1, upperbound1, lowerbound2, upperbound2, threshold, precision):
+        self.lowerbound1=lowerbound1
+        self.upperbound1=upperbound1
+        self.lowerbound2=lowerbound2
+        self.upperbound2=upperbound2
+        self.threshold=threshold
+        self.precision=precision
+        self.error_prob=0
+
+    def compute(self):
+        gmpy2.get_context().precision=self.precision
+        x=mpfr(self.threshold)
+        ceilx=gmpy2.next_above(x)
+        floorx=gmpy2.next_below(x)
+        gmpy2.get_context().precision=53
+        X=UniformDistr(self.lowerbound1,self.upperbound1)
+        Y=UniformDistr(self.lowerbound2,self.upperbound2)
+        Z=X+Y
+        self.error_prob=(((ceilx-x)/(ceilx-floorx))*Z.get_piecewise_pdf().integrate(x,ceilx))
