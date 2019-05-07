dae8982ad3d83cfc1f016e45a8f339ea58893509
Dahlqvist <fpd09@ic.ac.uk>
2019-05-07 14:20:24 +0100

    Changed class structure, corrected ceilx, floorx

--- Code/simple_tests.py
+++ Code/simple_tests.py
@@ -3,22 +3,15 @@ import gmpy2
 from gmpy2 import mpfr
 import matplotlib.pyplot as plt
 
-class TestUniformVariable:
-
-    def __init__(self, lowerbound, upperbound, threshold, precision):
-        self.lowerbound=lowerbound
-        self.upperbound=upperbound
+# An class for simple test programs
+class Test:
+    def __init__(self, threshold, precision):
         self.threshold=threshold
         self.precision=precision
         self.error_prob=0
 
     def compute(self):
-        gmpy2.get_context().precision=self.precision
-        x=mpfr(self.threshold)
-        y=gmpy2.next_above(x)
-        gmpy2.get_context().precision=53
-        X=UniformDistr(self.lowerbound,self.upperbound)
-        self.error_prob=(X.get_piecewise_pdf().integrate(x,y))/(X.get_piecewise_pdf().integrate(x,self.upperbound))
+        raise NotImplementedError("Method not implemented, please implement derived class")
 
     def plot_against_precision(self,minprecision,maxprecision):
         prec=[]
@@ -46,7 +39,27 @@ class TestUniformVariable:
         plt.ylabel('probability of error (%)')
         plt.show()
 
-class TestSumUniformVariable:
+# A class for testing a variable against a threshold
+class TestUniformVariable(Test):
+
+    def __init__(self, lowerbound, upperbound, threshold, precision):
+        self.lowerbound=lowerbound
+        self.upperbound=upperbound
+        self.threshold=threshold
+        self.precision=precision
+        self.error_prob=0
+
+    def compute(self):
+        gmpy2.get_context().precision=self.precision
+        x=mpfr(self.threshold)
+        ceilx=gmpy2.next_above(x)
+        gmpy2.get_context().precision=53
+        ceilx=(x+ceilx)/2
+        X=UniformDistr(self.lowerbound,self.upperbound)
+        self.error_prob=(X.get_piecewise_pdf().integrate(x,ceilx))/(X.get_piecewise_pdf().integrate(x,self.upperbound))
+
+# A class for testing the sum of two variables against a threshold
+class TestSumUniformVariable(Test):
 
     def __init__(self, lowerbound1, upperbound1, lowerbound2, upperbound2, threshold, precision):
         self.lowerbound1=lowerbound1
@@ -63,6 +76,8 @@ class TestSumUniformVariable:
         ceilx=gmpy2.next_above(x)
         floorx=gmpy2.next_below(x)
         gmpy2.get_context().precision=53
+        ceilx=(x+ceilx)/2
+        floorx=(floorx+x)/2
         X=UniformDistr(self.lowerbound1,self.upperbound1)
         Y=UniformDistr(self.lowerbound2,self.upperbound2)
         Z=X+Y
