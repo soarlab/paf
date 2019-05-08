f108f6d6bd8862e30d2d9bd8c13833c010cdc2ab
Dahlqvist <fpd09@ic.ac.uk>
2019-05-08 14:44:46 +0100

    Towards the pdf of exp(x)

--- Code/main.py
+++ Code/main.py
@@ -1,15 +1,26 @@
 import simple_tests
-
+from pacal import *
+import time
 # test1=simple_tests.TestUniformVariable(0,100,32,10)
 # test1.plot_against_precision(4,32)
 # test1.precision=10
 # test1.plot_against_threshold()
 
-test1=simple_tests.TestUniformVariable(0,2,0.75,10)
-test1.compute()
-test2=simple_tests.TestSumUniformVariable(0,1,0,1,0.75,10)
-test2.compute()
-print(test1.error_prob)
-print(test2.error_prob)
-test1.plot_against_precision(4,32)
-test2.plot_against_precision(4,32)
+#test1=simple_tests.TestUniformVariable(0,2,0.75,10)
+#test1.compute()
+#test2=simple_tests.TestSumUniformVariable(0,1,0,1,0.75,10)
+#test2.compute()
+#print(test1.error_prob)
+#print(test2.error_prob)
+#test1.plot_against_precision(4,32)
+#test2.plot_against_precision(4,32)
+
+start = time. time()
+#X =
+Y=0.01*UniformDistr(0,1)
+for i in range(1,80):
+    Y=Y+0.01*UniformDistr(0,1)
+Y.plot()
+#show()
+end = time. time()
+print(end - start)
