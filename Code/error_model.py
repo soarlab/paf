e75c7a15d24af01b037a301d27726fbbca6e8153
Dahlqvist <fpd09@ic.ac.uk>
2019-06-11 18:12:16 +0100

    Implemented a pdf and cdf for relative error

--- Code/error_model.py
+++ Code/error_model.py
@@ -4,6 +4,7 @@ import numpy as np
 from pychebfun import Chebfun
 import gmpy2
 from gmpy2 import mpfr
+import random
 
 class ErrorModel:
 
@@ -12,7 +13,51 @@ class ErrorModel:
         self.precision=precision
         self.minexp=minexp
         self.maxexp=maxexp
-        self.cdf=Chebfun.from_function(lambda t:self.__getcdf(t))
+        #for i in range(1,20):
+        #    x=self.__getpdf(-1.0+i*0.1)
+        #    print(x)
+        #pdf=Chebfun.from_function(lambda t:self.__getpdf(t),N=10)
+        #print(repr(pdf))
+        #print(self.__getcdf(1))
+        print(self.__getcdf(-1))
+        #for i in range(1,20):
+        #    x=self.__getcdf(-1.0+i*0.1)
+        #    print('f('+repr(-1.0+i*0.1)+')='+repr(x))
+        #cdf=Chebfun.from_function(lambda t:self.__getcdf(t),N=100)
+
+
+    def __getpdf(self, t):
+        #if t<-1 or t>1:
+        #    raise Exception('|t| should not exceed 1. The value of x was: {}'.format(t))
+        ctx=gmpy2.get_context()
+        ctx.precision=self.precision
+        ctx.emin=self.minexp
+        ctx.emax=self.maxexp
+        eps=2**-self.precision
+        x=gmpy2.next_above(gmpy2.inf(-1))
+        sum=0.0
+        pdf=self.inputdistribution.get_piecewise_pdf()
+        while gmpy2.is_finite(x):
+            ctx.precision=53
+            ctx.emin=-1023
+            ctx.emax=1023
+            #z=random.random()
+            if x<0:
+                y=float(x)
+                sum=sum+pdf(y/(1+t*eps))*(-y*eps/(1-t*eps)**2)
+                #if z<0.03:
+                #    print('at: ' + repr(x) +'       fct:'+repr(pdf(-y/(1+t*eps))*(y*eps/(1+t*eps)**2))+'        sum: '+repr(sum))
+            elif x>0:
+                y=float(x)
+                sum=sum+pdf(y/(1-t*eps))*(y*eps/(1-t*eps)**2)
+                #if z<0.03:
+                #    print('at: ' + repr(x) +'       fct:'+repr(pdf(y/(1-t*eps))*(y*eps/(1-t*eps)**2))+'        sum: '+repr(sum))
+            ctx.precision=self.precision
+            ctx.emin=self.minexp
+            ctx.emax=self.maxexp
+            x=gmpy2.next_above(x)
+        return(sum)
+        #print(sum)
 
 
     def __getcdf(self,t):
@@ -20,19 +65,36 @@ class ErrorModel:
         ctx.precision=self.precision
         ctx.emin=self.minexp
         ctx.emax=self.maxexp
+        eps=2**-(self.precision)
         x=gmpy2.next_above(gmpy2.inf(-1))
-        while gmpy2.is_finite(x):
+        y=gmpy2.next_above(x)
+        z=gmpy2.next_above(y)
+        sum=0.0
+        while gmpy2.is_finite(y):
             ctx.precision=53
-            ctx.emin=-1073741823
-            ctx.emax=1073741823
-            if x<0:
-                sum+=self.inputdistribution.get_piecewise_pdf().integrate(float(x)/(1-t),float(x)/(1+t))
-            elif x=0:
-                sum+=0
-            else:
-                sum+=self.inputdistribution.get_piecewise_pdf().integrate(float(x)/(1+t),float(x)/(1-t))
+            ctx.emin=-1023
+            ctx.emax=1023
+            xx=float(x)
+            yy=float(y)
+            zz=float(z)
+            d=yy-(yy-xx)/2
+            if yy<0:
+                u=max(d,min(yy/(1+t*eps), yy+(zz-yy)/2))
+                tmp=self.inputdistribution.cdf(u)-self.inputdistribution.cdf(d)
+                sum+=tmp
+                if tmp!=0:
+                    print('at: '+repr(d)+' < ' + repr(yy)+' < ' +repr(u)+'       fct:'+repr(tmp)+'        sum: '+repr(sum))
+            elif yy>0:
+                u=max(d,min(yy/(1-t*eps), yy+(zz-yy)/2))
+                tmp=self.inputdistribution.cdf(u)-self.inputdistribution.cdf(d)
+                sum+=tmp
+                if tmp!=0:
+                    print('xx='+repr(xx)+', yy='+repr(yy)+', zz='+repr(zz)+', rel err='+repr(yy/(1-t*eps)))
+                    print('at: '+repr(d)+' < ' + repr(yy)+' < ' +repr(u)+'   '+repr(yy+(zz-yy)/2)+'       fct:'+repr(tmp)+'        sum: '+repr(sum))
             ctx.precision=self.precision
             ctx.emin=self.minexp
             ctx.emax=self.maxexp
             x=gmpy2.next_above(x)
+            y=gmpy2.next_above(x)
+            z=gmpy2.next_above(y)
         return sum
--- Code/main.py
+++ Code/main.py
@@ -1,17 +1,32 @@
 import simple_tests
+import matplotlib.pyplot as plt
+from error_model import ErrorModel
 from stats import plot_error
 import pacal
 import time
 
-plot_error(pacal.NormalDistr(0.9,0.1),11,100000)
-plot_error(pacal.UniformDistr(0.9,1),11,100000)
+def test_error_model():
+    error=ErrorModel(pacal.UniformDistr(-100,100),10,-14,15)
 
 
 
-# test1=simple_tests.TestUniformVariable(0,1,0.25,10)
-# test1.plot_against_precision(4,32)
-# test1.precision=10
-# test1.plot_against_threshold()
+def test_plot_error():
+    plot_error(pacal.NormalDistr(0.9,0.1),11,100000)
+    plot_error(pacal.UniformDistr(0.9,1),11,100000)
+
+def test_simple_tests():
+    test1=simple_tests.TestUniformVariable(0,1,0.25,10)
+    test1.plot_against_precision(4,32)
+    test1.precision=10
+    test1.plot_against_threshold()
+
+
+
+#main:
+start = time.time()
+test_error_model()
+end = time.time()
+print('Elapsed time:'+repr(end - start)+'s')
 
 #test1=simple_tests.TestUniformVariable(0,2,0.75,10)
 #test1.compute()
@@ -21,13 +36,3 @@ plot_error(pacal.UniformDistr(0.9,1),11,100000)
 #print(test2.error_prob)
 #test1.plot_against_precision(4,32)
 #test2.plot_against_precision(4,32)
-
-# start = time. time()
-# #X =
-# Y=0.01*UniformDistr(0,1)
-# for i in range(1,80):
-#     Y=Y+0.01*UniformDistr(0,1)
-# Y.plot()
-# #show()
-# end = time. time()
-# print(end - start)
