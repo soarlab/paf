1405b2483ecb3070ebed50c983716d478b4dc188
rospoly <salvia.rocco@gmail.com>
2020-04-26 11:26:21 -0600

    Rocco: beta distribution for dot product

--- creator.py
+++ creator.py
@@ -1,13 +1,13 @@
 input_file=open("./test.txt","w+")
 
 declarations=""
-for i in range(0,480):
-	declarations=declarations+"x"+str(i)+":N(0.0,1.0), "+"y"+str(i)+":N(0.0,1.0), "
+for i in range(0,5):
+	declarations=declarations+"x"+str(i)+":B(2.0,2.0), "+"y"+str(i)+":B(2.0,2.0), "
 
 declarations=declarations[:-2]+"\n"
 
 expr="(x0*y0)"
-for i in range(1,480):
+for i in range(1,5):
 	expr="("+expr+"+(x"+str(i)+"*y"+str(i)+")"+")"
 
 final=declarations+expr
