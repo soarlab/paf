e0ea8d4ad1e1a788954617f3283877429c4d2070
rospoly <salvia.rocco@gmail.com>
2020-05-07 11:29:41 -0600

    Rocco: constant values

--- Higham/3_Dot_Product_Constants/3/plotter.py
+++ Higham/3_Dot_Product_Constants/3/plotter.py
@@ -8,17 +8,23 @@ plt.figure()
 x=open("./3_x.txt","r").read().split()
 x=list(map(int, x))
 
+markers=[".","v","p","s","x"]
+i=0
 for folder in os.listdir("./"):
+	print(folder)
 	if "3_" in folder and ".txt" in folder and not "3_x" in folder:
 		tmp=open("./"+folder,"r").read()
 		values=list(map(float, tmp.split()))
-		plt.plot(x, values, label=folder.split("_")[1], linewidth=3)
+		#print(folder)
+		#print(values)
+		plt.plot(x, values, label=folder.split("_")[1], linewidth=2, marker=markers[i], markersize=12)
+		i=i+1
 
-plt.title("Dot Product (Constants)")
-plt.xlabel('N')
+plt.title("inner product (constant entries)")
+plt.xlabel('n')
 plt.yscale('log')
 plt.xscale('log')
-plt.ylabel('Relative Error')
+plt.ylabel('relative error')
 #plt.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
 plt.legend()
 plt.show()
