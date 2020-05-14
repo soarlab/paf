a1065a305871fed80e0d2b9f3082eaefd84c4556
rospoly <salvia.rocco@gmail.com>
2020-05-14 17:07:42 -0600

    Rocco plotter fix

--- Higham/Dot_Product_Variables/1/gaussian/plotter.py
+++ Higham/Dot_Product_Variables/1/gaussian/plotter.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
-plt.rcParams.update({'font.size': 33})
+plt.rcParams.update({'font.size': 35})
 plt.figure(figsize=(17, 10))
 
 x=open("./1_x.txt","r").read().split()
@@ -11,8 +11,7 @@ x=list(map(int, x))
 markers=[".","v","p","s","x"]
 i=0
 
-lambda_="1"
-
+#lambda_="1"
 #for folder in os.listdir("./"):
 #	if "1_" in folder and ".txt" in folder and not "1_x" in folder:
 
@@ -20,9 +19,13 @@ tmp=open("./1_detbound.txt","r").read()
 values=list(map(float, tmp.split()))
 plt.plot(x, values, label="detbound", linewidth=3, marker=".", markersize=30)
 
-tmp=open("./1_probound_lambda_"+lambda_+".txt","r").read()
+tmp=open("./1_probound_lambda_5.txt","r").read()
 values=list(map(float, tmp.split()))
-plt.plot(x, values, label="probound", linewidth=3, marker="v", markersize=20)
+plt.plot(x, values, label="probound-lambda-5", linewidth=3, marker="^", markersize=20)
+
+tmp=open("./1_probound_lambda_1.txt","r").read()
+values=list(map(float, tmp.split()))
+plt.plot(x, values, label="probound-lambda-1", linewidth=3, marker="v", markersize=20)
 
 tmp=open("./1_paf.txt","r").read()
 values=list(map(float, tmp.split()))
@@ -42,9 +45,9 @@ plt.yscale('log')
 plt.xscale('log')
 plt.ylabel('relative error')
 #plt.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
-plt.legend(frameon=False)
+plt.legend(frameon=False, bbox_to_anchor=(-0.02,1.04), loc="upper left", labelspacing=.3)
 
-plt.savefig("normal_lambda_"+lambda_+".png", bbox_inches = 'tight',
+plt.savefig("normal_lambda_1_and_5.png", bbox_inches = 'tight',
     pad_inches = 0)
 
 plt.show()
