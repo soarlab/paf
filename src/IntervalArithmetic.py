482cfa32d1be9d12e7be7fb65b31295db6f8d027
rospoly <salvia.rocco@gmail.com>
2020-09-24 08:53:51 -0600

    Rocco: interval arithmetic library is safe wrt the rounding error

--- src/IntervalArithmetic.py
+++ src/IntervalArithmetic.py
@@ -1,7 +1,8 @@
 from interval import interval, inf, imath
+from mpmath import iv
 
 def perform_interval_operation(left_lower, left_upper, operator, right_lower, right_upper):
-    left_int = interval([left_lower, left_upper])
-    right_int = interval([right_lower, right_upper])
+    left_int = iv.mpf([left_lower, left_upper])
+    right_int = iv.mpf([right_lower, right_upper])
     res=eval("left_int"+operator+"right_int")
-    return res[0].inf, res[0].sup
+    return eval(str(res.a))[0], eval(str(res.b))[1]
