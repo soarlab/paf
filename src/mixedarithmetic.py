2d007f86210a19345e6f6fa3aaea6286b0711575
rospoly <salvia.rocco@gmail.com>
2020-11-04 14:01:11 -0700

    From DS to PBox and viceversa

--- src/mixedarithmetic.py
+++ src/mixedarithmetic.py
@@ -1,5 +1,4 @@
 import bisect
-import copy
 from decimal import Decimal
 
 import gmpy2
@@ -7,12 +6,13 @@ import numpy as np
 
 from AffineArithmeticLibrary import AffineInstance, AffineManager
 from IntervalArithmeticLibrary import Interval
-from plotting import plot_operation, plotCDFdiscretization, plot_boxing
-from project_utils import round_number_down_to_digits, round_number_up_to_digits, dec2Str, round_down, round_up, \
-    round_near
-from setup_utils import gap_cdf_regularizer, digits_for_discretization, digits_for_cdf, delta_error_computation
+from plotting import plot_operation, plot_boxing
+from project_utils import round_number_down_to_digits, dec2Str, round_near
+from setup_utils import digits_for_discretization, digits_for_cdf, delta_error_computation
 
-
+'''
+A PBox consists in a domain interval associated with a probability (CDF) interval
+'''
 class PBox:
     def __init__(self, interval, cdf_low, cdf_up):
         self.interval=interval
@@ -30,7 +30,9 @@ class PBox:
         tmp=Interval(self.cdf_low, self.cdf_up, True, True)\
             .addition(Interval(pbox.cdf_low, pbox.cdf_up, True, True))
         return PBox(None, tmp.lower, tmp.upper)
-
+'''
+In MixedArithmetic we combine Rigorous Interval Arithmetic with Rigorous Affine Arithmetic.
+'''
 class MixedArithmetic:
     def __init__(self, lower, upper, intervals):
         self.intervals = intervals
@@ -42,6 +44,9 @@ class MixedArithmetic:
         self.include_upper=None
         self.get_best_bounds()
 
+    '''
+    Compute the bounds using AA and IA and take the best.
+    '''
     def get_best_bounds(self):
         if Decimal(self.intervals[0].interval.lower)>Decimal(self.affine.interval.lower):
             self.lower=self.intervals[0].interval.lower
@@ -95,6 +100,9 @@ def createPairsFromBounds(values):
             tmp.remove(val)
     return ret
 
+'''
+It converts a set of PBox with PDFs to a Dempster-Shafer Structure
+'''
 def from_PDFS_PBox_to_DSI(insiders, evaluation_points):
     insiders_low = sorted(insiders, key=lambda x: Decimal(x.interval.lower), reverse=False)
     insiders_low_value = [Decimal(x.interval.lower) for x in insiders_low]
@@ -107,10 +115,10 @@ def from_PDFS_PBox_to_DSI(insiders, evaluation_points):
     val_cdf_low = []
     val_cdf_up = []
 
-    res_ub = Decimal("0")#Interval("0.0", "0.0", True, True, digits_for_cdf)
-    acc_ub = Decimal("0")#Interval("0.0", "0.0", True, True, digits_for_cdf)
-    res_lb = Decimal("0")#Interval("0.0", "0.0", True, True, digits_for_cdf)
-    acc_lb = Decimal("0")#Interval("0.0", "0.0", True, True, digits_for_cdf)
+    res_ub = Decimal("0")
+    acc_ub = Decimal("0")
+    res_lb = Decimal("0")
+    acc_lb = Decimal("0")
 
     previous_index_lower=0
     previous_index_upper=0
@@ -124,8 +132,6 @@ def from_PDFS_PBox_to_DSI(insiders, evaluation_points):
                 exit(-1)
             if Decimal(inside.interval.lower) <= ev_point:
                 res_ub = res_ub + Decimal(inside.cdf_low)
-            #elif Decimal(inside.interval.lower) == ev_point and inside.interval.include_lower:
-                #res_ub = res_ub + Decimal(inside.cdf_low)
             acc_ub=acc_ub+Decimal(inside.cdf_low)
         previous_index_lower=index
 
@@ -137,8 +143,6 @@ def from_PDFS_PBox_to_DSI(insiders, evaluation_points):
                 exit(-1)
             if Decimal(inside.interval.upper) <= ev_point:
                 res_lb=res_lb+Decimal(inside.cdf_low)
-            #elif Decimal(inside.interval.upper) == ev_point and inside.interval.include_upper:
-                #res_lb=res_lb+Decimal(inside.cdf_low)
             acc_lb=acc_lb+Decimal(inside.cdf_low)
         previous_index_upper=index
 
@@ -150,14 +154,18 @@ def from_PDFS_PBox_to_DSI(insiders, evaluation_points):
 
     return edge_cdf, val_cdf_low, val_cdf_up
 
-
+'''
+It converts a set of PBox with CDFs to a Dempster-Shafer Structure
+'''
 def from_CDFS_PBox_to_DSI(insiders, evaluation_points):
 
+    #First check wheter the insiders sum up to 1
     sum=Decimal("0")
     for box in insiders:
         sum=sum+Decimal(box.cdf_up)-Decimal(box.cdf_low)
     print("Check PDF from_CDFS_PBox_to_DSI: "+str(sum))
 
+    #Assign to the pbox the probability mass associated with the interval
     for box in insiders:
         pdf_val=Decimal(box.cdf_up)-Decimal(box.cdf_low)
         box.cdf_low=dec2Str(pdf_val)
@@ -170,21 +178,6 @@ def convertListToDecimals(my_list):
     for element in my_list:
         ret_list.append(Decimal(element))
     return ret_list
-
-
-def adjust_values_sum_one(values_lower, values_upper):
-    for index_low in range(1,len(values_lower)):
-        low_value=values_lower[index_low]
-        if values_upper[index_low-1] < low_value:
-            values_upper[index_low - 1] = low_value
-    return values_lower, values_upper
-
-def adjust_max_and_min(values_lower, values_upper):
-    #values_lower[0]=Decimal("0.0")
-    #values_upper[0]=Decimal("0.0")
-    values_lower[-1] = Decimal("1.0")
-    values_upper[-1] = Decimal("1.0")
-    return values_lower, values_upper
 
 def from_DSI_to_PBox_with_Delta(edges_lower, values_lower, edges_upper, values_upper):
     ret_list=[]
@@ -209,15 +202,11 @@ def from_DSI_to_PBox_with_Delta(edges_lower, values_lower, edges_upper, values_u
     ret_list.append(last_box)
     return ret_list
 
-
 def from_DSI_to_PBox(edges_lower, values_lower, edges_upper, values_upper):
     edges_lower=convertListToDecimals(edges_lower)
     values_lower=convertListToDecimals(values_lower)
     edges_upper=convertListToDecimals(edges_upper)
     values_upper=convertListToDecimals(values_upper)
-
-    #values_lower,values_upper=adjust_values_sum_one(values_lower,values_upper)
-    #values_lower,values_upper=adjust_max_and_min(values_lower, values_upper)
 
     ret_list=[]
     if not edges_lower==edges_upper:
@@ -228,12 +217,9 @@ def from_DSI_to_PBox(edges_lower, values_lower, edges_upper, values_upper):
     pair_values_upper=createPairsFromBounds(values_upper)
 
     for pair_value_upper in pair_values_upper:
-        #the last element of the upper bound does not connect!
-        #if pair_value_upper[2]==len(edges_lower)-1:
-        #    continue
-        #we use bisect left because in case of multiple values we pick the first of them
         index = bisect.bisect_left(values_lower, pair_value_upper[0])
         pair_value_upper[1] = True
+
         #in case we go out of bounds we take the last one
         if index==len(values_lower):
             index=index-1
@@ -267,7 +253,6 @@ def from_DSI_to_PBox(edges_lower, values_lower, edges_upper, values_upper):
                                   True, False, digits_for_discretization), "", dec2Str(pair_value_lower[0])))
     #sort by cdf, in case they are equal sort by lower bound
     ret_list.sort(key=lambda x: (float(x.cdf_up), float(x.interval.lower)))
-    pboxes_cdfs=[]
     prec="0.0"
     for elem in ret_list:
         if not Decimal(prec)==Decimal(elem.cdf_up):
@@ -279,11 +264,11 @@ def from_DSI_to_PBox(edges_lower, values_lower, edges_upper, values_upper):
     ret_list= [x for x in ret_list if not x.cdf_low=="REMOVE"]
     ret_list[-1].interval.include_upper=True
 
-    plot_operation(edges_lower,values_lower,values_upper)
-    plot_boxing(ret_list)
+    #plot_operation(edges_lower,values_lower,values_upper)
+    #plot_boxing(ret_list)
+
     sum=0
     for box in ret_list:
         sum=sum+Decimal(box.cdf_up)-Decimal(box.cdf_low)
     print("Check PDF from bounds to intervals: "+str(sum))
-    #plotCDFdiscretization(ret_list)
     return ret_list
